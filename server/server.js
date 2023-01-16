3cca008c9abdda3c9e10b8f784134c1841f3d58f
mgetz34 <mgetz340@gmail.com>
2023-01-15 17:10:38 -0700

    added apollo server const and middleware code to server.js, removed routes and consts, require in typeDefs, Resolvers, and apollo server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +20,25 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
