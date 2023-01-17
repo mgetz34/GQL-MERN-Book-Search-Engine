50c3521cd96fdbb4f73e4aa8d784c928f7f3da9e
mgetz34 <mgetz340@gmail.com>
2023-01-17 12:16:39 -0700

    pushing project files

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 import { LOGIN_USER } from "../utils/mutations";
 import { useMutation } from "@apollo/client";
-// import { loginUser } from '../utils/API';
 import Auth from "../utils/auth";
 
 const LoginForm = () => {
